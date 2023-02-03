abaf5755b3314e9ffa99c98008dbb8bf05dff728
Eduardo Diaz <me@iamedu.email>
2023-02-02 21:02:51 -0800

    Infra

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {}
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
