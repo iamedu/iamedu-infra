7a90613238eef798cdf98daf91a73f8b274b8a86
Eduardo Diaz <me@iamedu.email>
2023-02-03 06:48:19 -0800

    Adding backend api

--- digitalocean.tf
+++ digitalocean.tf
@@ -2,3 +2,27 @@ resource "digitalocean_container_registry" "iamedu_registry" {
   name                   = "iamedu-registry"
   subscription_tier_slug = "starter"
 }
+
+resource "digitalocean_app" "website" {
+  spec {
+    name   = "iamedu-website"
+    region = "sfo3"
+
+    service {
+      name      = "api"
+      http_port = 8080
+
+      image {
+        registry_type = "DOCR"
+        repository    = "website/backend"
+        deploy_on_push {
+          enabled = true
+        }
+      }
+
+      routes {
+        path = "/api"
+      }
+    }
+  }
+}
